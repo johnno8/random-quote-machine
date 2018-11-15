40fd602c73fe300a2b3430d5dcfc47d5e2486559
John O Keeffe <jokeeffe@internal.onaware.com>
2018-11-15 10:37:21 +0000

    Change quote-box height

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ class App extends Component {
     super(props)
   }
 
-/*
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */
-
   render() {
     const q = { body: 'body of some quote', author: 'D. Writer' }
 
