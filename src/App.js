9ddc9cd4f308bc1ad04eddb013e0d4c14f40ffe1
John O Keeffe <jokeeffe09@gmail.com>
2018-11-21 17:16:34 +0000

    Add colour change with new-quote click

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const colors = ['#96ABA7', '#719e35', '#52ad9a', '#3b3a36', '#7d4627', '#89bdd3','#bd222f', '#173e43', '#22264b', '#5a5c51', '#aa863a', '#283018', '#7baa71', '#3d5094', '#e05038', '#e6af4b'];
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       quotes: [{ quote: 'fetching quotes...', author: ''}],
-      index: 0
+      index: 0,
+      color: colors[0]
     }
   }
 
   handleClick = () => {
     this.setState({
-      index: Math.floor(Math.random() * 102)
+      index: Math.floor(Math.random() * 102),
+      color: colors[Math.floor(Math.random() * 16)]
     });
   }
 
@@ -24,16 +28,19 @@ class App extends Component {
       .then(r => {
         this.setState({
           quotes: r.quotes,
-          index: Math.floor(Math.random() * 102)
+          index: Math.floor(Math.random() * 102),
+          color: colors[Math.floor(Math.random() * 6)]
         });
       }).catch(err => console.log(err))
   }
 
   render() {
-
     return (
-      <div className="App">
-        <QuoteBox q={this.state.quotes[this.state.index]} getIndex={this.handleClick}/>    
+      <div className="App" style={{backgroundColor: this.state.color}}>
+        <QuoteBox 
+          q={this.state.quotes[this.state.index]} 
+          getIndex={this.handleClick}
+          color={this.state.color}/>    
       </div>
     );
   } 
@@ -49,13 +56,13 @@ class QuoteBox extends Component {
     let tweetURL = 'https://twitter.com/intent/tweet?text="' + this.props.q.quote + '"' + ' - ' + this.props.q.author + '&hashtags=freecodecamp,randomquote';
     return (
       <div id="quote-box">
-        <div id="text-container">
+        <div id="text-container" style={{color: this.props.color}}>
           <div id="text">'{this.props.q.quote}'</div>
           <div id="author">- {this.props.q.author}</div>
         </div>
         <div id="quote-box-footer">
-          <a href={tweetURL} id="tweet-quote" target="_blank"><button class="tweet"><i className="fa fa-twitter"/> Tweet</button></a>
-          <button id="new-quote" onClick={this.props.getIndex}>New quote</button>
+          <a href={tweetURL} id="tweet-quote" target="_blank" rel="noopener noreferrer"><button className="tweet" style={{backgroundColor: this.props.color}}><i className="fa fa-twitter"/> Tweet</button></a>
+          <button id="new-quote" onClick={this.props.getIndex} style={{backgroundColor: this.props.color}}>New quote</button>
         </div>
       </div>
     )
